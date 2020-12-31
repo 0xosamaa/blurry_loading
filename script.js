c0493397d3ea0064b61447ffc20399a183dbaf81
m7md <0xm.osamaa@gmail.com>
2020-12-31 03:25:02 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const bg = document.querySelector('.bg')
+const per = document.querySelector('.loading-text')
+let load = 0
+
+let int = setInterval(blurring, 30)
+
+function blurring() {
+    load++
+    if (load > 99){
+        clearInterval(int)
+    }
+    per.innerText = `${load}%`
+    per.style.opacity = (100 - load) / 100
+    bg.style.filter = `blur(${(100 - load) / (100 / 30)}px)`
+}
